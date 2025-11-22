a88d96d2dacbdd3c8e48fdc8f4eeafcd04779315
raygdev <raymond.greeley.dev@gmail.com>
2025-11-22 17:32:07 -0500

    Add boilerplate setup

---
+++ marketing/config/webpack.dev.js
@@ -0,0 +1,19 @@
+const { merge } = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const commonConfig = require('./webpack.common')
+
+const devConfig = {
+    mode: 'development',
+    devServer: {
+        port: 8081,
+        historyApiFallback: true,
+        publicPath: '/'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, devConfig)
