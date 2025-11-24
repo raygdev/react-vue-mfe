8bfe58a3c1fa15e9d0a12659d7867da69ff91358
raygdev <raymond.greeley.dev@gmail.com>
2025-11-23 20:15:48 -0500

    Add auth boilerplate and configuration

---
+++ auth/src/bootstrap.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from 'react-dom'
+import { createMemoryHistory, createBrowserHistory } from 'history'
+import App from './App'
+
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory()
+
+  if(onNavigate) {
+    history.listen(onNavigate)
+  }
+
+
+  ReactDOM.render(<App history={history}/>,el)
+
+    return {
+      onParentNavigate: (location) => {
+        if(history.location.pathname !== location.pathname) {
+            history.push(location.pathname)
+        }
+      }
+    }
+}
+
+if(process.env.NODE_ENV === 'development') {
+    const el = document.querySelector('#_auth-dev-root')
+    if(el) {
+        mount(el, { defaultHistory: createBrowserHistory()})
+    }
+}
+
+
+export { mount }
