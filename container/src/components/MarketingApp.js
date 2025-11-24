c9f6c54c07269061920efe8bcd0142049314d463
raygdev <raymond.greeley.dev@gmail.com>
2025-11-23 19:45:00 -0500

    Handle multi-tier navigation between container and subapps

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,11 +1,21 @@
 import { mount } from 'marketing/Marketing'
 import React, { useEffect, useRef } from 'react'
+import { useHistory } from 'react-router-dom'
 
 export default function MarketingApp() {
     const ref = useRef(null)
+    const history = useHistory()
 
     useEffect(() => {
-        mount(ref.current)
+       const { onParentNavigate } = mount(ref.current, {
+          onNavigate: (location) => {
+            if(history.location.pathname !== location.pathname) {
+                history.push(location.pathname)
+            }
+          }
+        })
+
+        history.listen(onParentNavigate)
     }, [])
 
     return <div ref={ref}></div>
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,18 +1,31 @@
 import React from "react";
 import ReactDOM from 'react-dom'
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, createBrowserHistory } from 'history'
 import App from './App'
 
-const mount = (el) => {
-  const history = createMemoryHistory()
-  ReactDOM.render(<App history={history}/>,
-    el) 
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory()
+
+  if(onNavigate) {
+    history.listen(onNavigate)
+  }
+
+
+  ReactDOM.render(<App history={history}/>,el)
+
+    return {
+      onParentNavigate: (location) => {
+        if(history.location.pathname !== location.pathname) {
+            history.push(location.pathname)
+        }
+      }
+    }
 }
 
 if(process.env.NODE_ENV === 'development') {
     const el = document.querySelector('#_marketing-dev-root')
     if(el) {
-        mount(el)
+        mount(el, { defaultHistory: createBrowserHistory()})
     }
 }
 
