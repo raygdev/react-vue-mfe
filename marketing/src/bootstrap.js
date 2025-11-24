2e83f544c9186c4ac68c146516f54e60264f499e
raygdev <raymond.greeley.dev@gmail.com>
2025-11-23 19:15:40 -0500

    Implement memory router for marketing

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from 'react-dom'
-
+import { createMemoryHistory } from 'history'
 import App from './App'
 
 const mount = (el) => {
-  ReactDOM.render(<App />,
+  const history = createMemoryHistory()
+  ReactDOM.render(<App history={history}/>,
     el) 
 }
 
