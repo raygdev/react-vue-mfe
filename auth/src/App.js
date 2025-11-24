49d135f68b3ec976a6fee848606eb2c3a2b1cc9e
raygdev <raymond.greeley.dev@gmail.com>
2025-11-23 20:19:21 -0500

    Remove copied over imports

--- auth/src/App.js
+++ auth/src/App.js
@@ -2,9 +2,6 @@ import React from "react";
 import { Switch, Route, Router } from 'react-router-dom'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
-import Landing from './components/Landing'
-import Pricing from './components/Pricing'
-
 const generateClassName = createGenerateClassName({
   productionPrefix: 'au'
 })
